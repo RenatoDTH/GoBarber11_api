25192023f0e98a55fb353c223769ff26208cee58
Renato Castro <renatocastro02@hotmail.com>
2020-09-05 14:14:42 -0300

    Autenticação

---
+++ src/config/auth.ts
@@ -0,0 +1,6 @@
+export default {
+  jwt: {
+    secret: 'de0d4f14f495a77ab0099613003508fc',
+    expiresIn: '1d',
+  },
+};
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,10 +1,12 @@
 import { Router } from 'express';
 import appointmentsRoutes from './appointments.routes';
-import useRouter from './users.routes';
+import usersRouter from './users.routes';
+import sessionsRouter from './sessions.routes';
 
 const routes = Router();
 
 routes.use('/appointments', appointmentsRoutes);
-routes.use('/users', useRouter);
+routes.use('/users', usersRouter);
+routes.use('/sessions', sessionsRouter);
 
 export default routes;
