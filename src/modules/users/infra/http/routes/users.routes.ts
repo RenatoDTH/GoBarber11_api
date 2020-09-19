2fc997dd2248fdbeddcba9a2dae7fe77ff7b37ba
Renato Castro <renatocastro02@hotmail.com>
2020-09-19 14:44:50 -0300

    Liskov e Dependency Inversion Principle

--- src/modules/users/infra/http/routes/users.routes.ts
+++ src/modules/users/infra/http/routes/users.routes.ts
@@ -1,6 +1,7 @@
 import { Router } from 'express';
 import multer from 'multer';
 import uploadConfig from '@config/upload';
+import UsersRepository from '@modules/users/infra/typeorm/repositories/UsersRepositories';
 import CreateUserService from '@modules/users/services/CreateUserService';
 import UpdateUserAvatarService from '@modules/users/services/UpdateUserAvatarService';
 import ensureAuthenticated from '../middlewares/ensureAuthenticated';
@@ -11,7 +12,9 @@ const upload = multer(uploadConfig);
 usersRouter.post('/', async (request, response) => {
   const { name, email, password } = request.body;
 
-  const createUser = new CreateUserService();
+  const UsersRepositories = new UsersRepository();
+
+  const createUser = new CreateUserService(UsersRepositories);
 
   const user = await createUser.execute({
     name,
@@ -29,7 +32,9 @@ usersRouter.patch(
   ensureAuthenticated,
   upload.single('avatar'),
   async (request, response) => {
-    const updateUserAvatar = new UpdateUserAvatarService();
+    const UsersRepositories = new UsersRepository();
+
+    const updateUserAvatar = new UpdateUserAvatarService(UsersRepositories);
 
     const user = await updateUserAvatar.execute({
       user_id: request.user.id,
