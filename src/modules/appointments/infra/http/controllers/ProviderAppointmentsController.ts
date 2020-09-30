0005244282e7217b7f801a35611126cd52b034e7
Renato Castro <renatocastro02@hotmail.com>
2020-09-30 13:42:01 -0300

    Agenda do prestador - fix

--- src/modules/appointments/infra/http/controllers/ProviderAppointmentsController.ts
+++ src/modules/appointments/infra/http/controllers/ProviderAppointmentsController.ts
@@ -4,7 +4,7 @@ import { container } from 'tsyringe';
 
 export default class ProviderAppointmentsController {
   public async index(request: Request, response: Response): Promise<Response> {
-    const { provider_id } = request.params;
+    const provider_id = request.user.id;
     const { day, month, year } = request.body;
 
     const listProviderAppointments = container.resolve(
